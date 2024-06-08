025d05a75a0bbb25fe9b897ade79753840447125
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-06-08 14:04:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function onesDigit(n) {
-  // Check if the input is a positive integer
-  if (!Number.isInteger(number) || number <= 0) {
-    throw new Error("Input must be a positive integer.");
-  }
-
-  // Return the ones digit of the number
-  return number % 10;
+   const number = Number(n);
+    if (!Number.isInteger(number) || number <= 0) {
+        throw new Error("Input must be a positive integer");
+    }
+    return number % 10;
 }
 
 // Do not change the code below
